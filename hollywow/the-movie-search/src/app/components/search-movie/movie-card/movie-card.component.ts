38b798114fadd348959ffb7a7f70438908c523bb
Kevin Dominguez <41652908+kevindm181998@users.noreply.github.com>
2022-06-06 00:35:15 -0500

    :art: general styles

--- hollywow/the-movie-search/src/app/components/search-movie/movie-card/movie-card.component.ts
+++ hollywow/the-movie-search/src/app/components/search-movie/movie-card/movie-card.component.ts
@@ -10,11 +10,13 @@ export class MovieCardComponent implements OnInit {
   @Input() imgUrl: string;
   @Input() title: string;
   @Input() releaseDate: string;
+  @Input() overview: string;
 
   constructor() { 
     this.imgUrl = "";
     this.title = "";
     this.releaseDate = "";
+    this.overview = "";
   }
 
   ngOnInit(): void {
