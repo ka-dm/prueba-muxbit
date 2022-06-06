2a77b8229ac0a9e918fbc560a8714b1f8030b05b
Kevin Dominguez <41652908+kevindm181998@users.noreply.github.com>
2022-06-06 01:07:41 -0500

    :books: document code

--- sort_and_search.py
+++ sort_and_search.py
@@ -1,4 +1,5 @@
-# Punto 2 : metodo que ordena una lista de manera recursiva
+# Punto 2 : metodo que ordena una lista de manera recursiva con el algoritmo merge sort
+# el cual se basa en la estrategia de dividir y juntar
 def merge_sort(lst):
     
     if len(lst) > 1:
@@ -39,6 +40,9 @@ def sort_list(lst):
     
     
 # Punto 3: metodo que crea el arbol binario de busqueda
+# Recibe una lista de numeros enteros
+# Devuelve un arbol binario de busqueda donde un nodo se representa de la siguiente manera:
+# [key, left_subtree, right_subtree]
 def create_bts(lst):
     tree = []
     if len(lst) == 0:
@@ -59,6 +63,9 @@ def create_bts(lst):
 
 
 # Punto 4: metodo que realiza la busqueda en el arbol binario de busqueda
+# basado en el la representacion del punto 3 para un arbol binario de busqueda
+# el cual recibe una lista de numeros enteros y un numero entero a buscar
+# y retorna True si el numero esta en el arbol binario de busqueda y la profundidad de la busqueda
 def search_bts(tree, key, depth=0):
     
     
@@ -82,12 +89,17 @@ def search_bts(tree, key, depth=0):
       
 if __name__ == '__main__':
     
+    # Lista de numeros enteros no ordenada
     my_list = [ 4, 7, 1, 5, 3, 6, 2]
+    
+    # Lista de numeros enteros ordenada
     output = sort_list(my_list)
     
+    # Inicializacion del arbol binario de busqueda
     bts_list = create_bts(output)
     print("Binary tree = ",bts_list, end="\n\n")   
     
+    # Busqueda de un numero en el arbol binario de busqueda
     search_bts(bts_list, 3)
     
     
